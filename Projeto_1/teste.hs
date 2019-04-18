963065389b022c3505d8535224996495bec70086
Gabriel Teixeira <gabrielbsb21@outlook.com>
2019-04-18 10:33:22 -0300

    testes

--- Projeto_1/teste.hs
+++ Projeto_1/teste.hs
@@ -10,7 +10,14 @@ type Medicamento = (Nome,Quantidade)
 type Medicamentos = [Medicamento]
 type Prescricao = (Nome,Horario,HorarioProximo)
 type PlanoMedicamento = [Prescricao]
+type Remedio = String --tipo de teste
+type Lista_remedio = [Remedio] --tipo de teste
 
 --testes da questao 1
+--remedio = ("dorflex", 5)
 
-adicionarMedicamento :: Medicamento -> Medicamentos -> Medicamentos
+--adicionarMedicamento :: Medicamento -> Medicamentos -> Medicamentos
+
+colocarMedicamento :: Remedio -> Lista_remedio --ate o momento pega um remedio e torna o mesmo na Lista_remedio
+colocarMedicamento z = z : [] -- a letra z recebe o remedio e adiciona na Lista_remedio
+
