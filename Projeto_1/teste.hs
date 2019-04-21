dc655a7cc20621d025813fb3222ff1d76e21a851
Gabriel Teixeira <gabrielbsb21@outlook.com>
2019-04-21 11:37:04 -0300

    Testes

--- Projeto_1/teste.hs
+++ Projeto_1/teste.hs
@@ -18,3 +18,13 @@ adicionarMedicamento :: Medicamento -> Medicamentos -> Medicamentos
 adicionarMedicamento nome [] = nome : [] -- nome e a variavel escolhida para receber os parametros, que no caso eh o nome do remedio e sua quantidade
 -- "x" representa a cabeca da lista, e "xs" representa todo o resto da lista
 
+
+--teste para adicionar um elemento numa lista de inteiro, recebe uma lista, depois um numero inteiro, e add esse numero na lista
+teste :: [Int] -> Int -> [Int]
+teste a b = b : a
+
+-- teste 2 pega a mesma funcao de cima, so que agora a lista e percorrida para verificar se esse elemento pertence a lista
+teste1 :: [Int] -> Int -> Bool
+teste1 [] _ = False
+teste1 (x:xs) b | (x == b) = True --sendo x a cabeca da lista e xs o corpo da lista
+                | otherwise = teste1 xs bs
