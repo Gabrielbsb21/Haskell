880f786b2e2517784e210852519822c38ea11ec2
Gabriel Teixeira da Silva <41963787+Gabrielbsb21@users.noreply.github.com>
2020-08-30 17:56:26 -0300

    Update 10.hs

--- Exercicios/10.hs
+++ Exercicios/10.hs
@@ -1,5 +1,5 @@
 
 
-inverte :: [a] -> [a] --recebe uma lista de qualquer tipo, e retorna uma lista
+inverte :: [a] -> [a] -- uma função recebe uma lista de qualquer tipo, e retorna uma lista
 inverte [] = [] --
 inverte (x:xs) = (inverte xs)++[x]
